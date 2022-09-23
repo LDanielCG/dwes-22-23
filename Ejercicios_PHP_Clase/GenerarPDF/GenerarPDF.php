d431ccc2730a79fee496b0c443828b3a7b3acc71
LDanielCG <luis.daniel.casgui@gmail.com>
2022-09-23 20:45:56 +0200

    CartaRecomendacionGenerarPDF_noCSS

--- Ejercicios_PHP_Clase/GenerarPDF/GenerarPDF.php
+++ Ejercicios_PHP_Clase/GenerarPDF/GenerarPDF.php
@@ -1,17 +1,6 @@
 <?php
 require('/home/ldaniel/Desktop/fpdf184/fpdf.php');
 
-function generarPdf(){
-    $pdf = new FPDF();
-    $pdf -> AddPage();
-    $pdf -> SetFont('Arial','B',18);
-    $pdf -> Cell(60,20, 'Nombre: ');
-    $pdf -> Cell(60,20, 'Empresa: ');
-
-    $pdf -> Cell(60,20, $Nombre);
-
-    $pdf -> Output();
-}
 
 
 
@@ -73,7 +62,39 @@ if ($error == true){
     $Empresa = $_GET['empresa'];
     $Representante = $_GET['representante'];
     $Fecha = $_GET['fecha'];
-    generarPdf();
+    generarPdf($Nombre,$Empresa,$Representante,$Fecha);
+}
+function generarPdf($Nb, $Em, $Rep, $Fec){
+    $pdf = new FPDF();
+    $pdf -> AddPage();
+    $pdf -> SetFont('Arial','B',18);
+    $pdf -> SetMargins(20,20,20);
+    $pdf -> Cell(0,20, utf8_decode('Carta de recomendación'),0,0,'C');
+    $pdf -> Ln();
+    $pdf -> SetFont('Arial','',11);
+    $pdf -> Cell(0,20, 'A quien corresponda,');
+    $pdf -> Ln();
+    $pdf -> MultiCell(0,7, utf8_decode('Yo, ' . $Rep . ', director de ' . $Em . ', hago constar mediante la presente que ' . $Nb . ', trabajó en esta empresa desde el 20 de enero de 2018 al 31 de mayo de 2019 en el departamento de informática, trabajando bajo mis órdenes directas.'));
+    $pdf -> Ln();
+    $pdf -> MultiCell(0,7, utf8_decode('Es por ello que es para mí un placer recomendar al Sr. ' . $Nb . ' para el puesto que ofertan en el departamento de informática de su empresa. Le puedo asegurar que incorporará un gran activo a su equipo, ya que tiene experiencia en el dicho campo. Además, es una persona muy trabajadora y muy confiable, en la que se pueden delegar tareas importantes.'));
+    $pdf -> Ln();
+    $pdf -> Cell(0,7, utf8_decode('Reciba un cordial saludo.'));
+    $pdf -> Ln();
+    $pdf -> Cell(0,7, utf8_decode('Atentamente'));
+    $pdf -> Ln();
+    $pdf -> Cell(0,7, utf8_decode($Rep));
+    $pdf -> Ln();
+    $pdf -> Cell(0,7, utf8_decode('Director de ' . $Em));
+    $pdf -> Ln();
+    $pdf -> Cell(0,7, utf8_decode('Firmado a día ' . $Fec),0,0,'R');
+
+
+
+
+
+
+
+    $pdf -> Output();
 }
 
 
@@ -84,8 +105,8 @@ if ($error == true){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
+    <head>
+        <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
