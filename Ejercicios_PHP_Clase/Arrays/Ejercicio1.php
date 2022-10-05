041f079363238f977eceeb6834b8a7743b6bdc78
LDanielCG <luis.daniel.casgui@gmail.com>
2022-10-05 12:42:14 +0200

    Ej1-3apartadosHechos

--- Ejercicios_PHP_Clase/Arrays/Ejercicio1.php
+++ Ejercicios_PHP_Clase/Arrays/Ejercicio1.php
@@ -16,8 +16,6 @@ $resultado = ["Señor Jorge", "Señora Bea", "Señor Paco", "Señora Amparo"];
 -->
 
 <?php
-
-
     $personas = [
         ["Jorge", 1],
         ["Bea", 0],
@@ -25,25 +23,23 @@ $resultado = ["Señor Jorge", "Señora Bea", "Señor Paco", "Señora Amparo"];
         ["Amparo", 0],
     ];
 
-    function modifPersonas($per, $prefijo){
-        echo "$prefijo $per";
-    }
-
-    foreach ($personas as $nom){
-        foreach($nom as $num){
-            if($num == 1){
-                echo 'señor'.$nom . '<br>';
-            }elseif($num == 0){
-                echo 'señora'.$nom . '<br>';
-            }
+    function modifPersonas($per){    
+        if($per[1] == 1){
+            return "Señor ".$per[0];
+        }else{
+            return "Señora ".$per[0];
         }
+        
+        //Con operador ternario
+        //return $per[1] == 1 ? "Señor ".$per[0] : "Señora ".$per[0];
     }
-   //print_r($personas);
 
+    array_walk($personas,'modifPersonas');
 
+    $conPrefijo = array_map('modifPersonas',$personas);
 
-
-
+    print_r($conPrefijo);
+    echo '<br><br><br>';
 ?>
 
 
@@ -53,14 +49,58 @@ $resultado = ["Señor Jorge", "Señora Bea", "Señor Paco", "Señora Amparo"];
 
 $comida = [
      0 => ["Banana", 3, 56],
-1 => ["Chuleta", 1, 256]
-2 => ["Pan", 1, 90]
-    ]
+    1 => ["Chuleta", 1, 256],
+    2 => ["Pan", 1, 90]
+]
     
-Utiliza la función map_reduce para calcular la cantidad de calorías de la comida anterior.
-
---
-
-Con el array de personas anterior, utiliza el array_filter para sacar un listado de Hombre y otro listado de mujeres.
+Utiliza la función array_reduce para calcular la cantidad 
+de calorías de la comida anterior.
 
 -->
+<?php
+    $comida = [
+        0 => ["Banana", 3, 56],
+        1 => ["Chuleta", 1, 256],
+        2 => ["Pan", 1, 90]
+    ];
+    
+    function producto($res,$comidas) {
+        $res += $comidas[1]*$comidas[2];
+        return $res;
+    }
+
+
+    $resultados = array_reduce($comida, "producto");
+    print_r('Calorias totales: '.$resultados);
+    echo '<br><br><br>';
+?>
+
+
+<!--
+Con el array de personas anterior, utiliza el 
+array_filter para sacar un listado de Hombre y otro listado de mujeres.
+-->
+<?php
+
+    $personas = [
+        ["Jorge", 1],
+        ["Bea", 0],
+        ["Paco", 1],
+        ["Amparo", 0],
+    ];
+
+    function hombres($impares){
+        return $impares[1] & 1;
+    }
+    function mujeres($pares){
+        return !($pares[1] & 1);
+    }
+
+    echo "<br>Hombres:<br>";
+    print_r(array_filter($personas, "hombres"));
+    echo "<br>Mujeres:<br>";
+    print_r(array_filter($personas, "mujeres"));
+?>
+
+
+
