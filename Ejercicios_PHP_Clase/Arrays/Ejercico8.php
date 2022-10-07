5db946171a71cb1a596501b492b1f8450155a954
LDanielCG <luis.daniel.casgui@gmail.com>
2022-10-07 18:44:58 +0200

    AlgoEnEl5YEl8

---
+++ Ejercicios_PHP_Clase/Arrays/Ejercico8.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+
+
+    $persona1 = [
+        "nombre" => "Luis Daniel",
+        "apellido" => "Casado Guimaraes",
+        "telefono" => "111222333",
+        "correo" => "Daniel@mail.com"
+    ];
+
+    $persona2 = [
+        "nombre" => "Lleison",
+        "apellido" => "Londoño Barreto",
+        "telefono" => "222333111",
+        "correo" => "Lleison@mail.com"
+    ];
+
+    $persona3 = [
+        "nombre" => "Mario",
+        "apellido" => "Sanchez Luque",
+        "telefono" => "333111222",
+        "correo" => "Mario@mail.com"
+    ];
+
+    $persona4 = [
+        "nombre" => "M-A-Y",
+        "apellido" => "GO",
+        "telefono" => "555000555",
+        "correo" => "5may@mail.com"
+    ];
+
+
+
+    $edades = ["25","22 ","24 ","29 ", "26","23","20","19","26","19","20","23"];
+
+
+
+
+
+
+?>
+
+</body>
+</html>
