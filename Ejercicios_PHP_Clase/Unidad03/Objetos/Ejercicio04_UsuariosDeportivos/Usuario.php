177ab6d6575b2c968547ec3b1ff7a90f9715b961
LDanielCG <luis.daniel.casgui@gmail.com>
2022-10-24 17:55:18 +0200

    SistemaNiveles_Works

--- Ejercicios_PHP_Clase/Unidad03/Objetos/Ejercicio04_UsuariosDeportivos/Usuario.php
+++ Ejercicios_PHP_Clase/Unidad03/Objetos/Ejercicio04_UsuariosDeportivos/Usuario.php
@@ -5,6 +5,7 @@
         private $deporte;
         private $nivel = 0;
         private $historicoPartidos = [];
+        private $historicoPartidosOG = [];
 
         function __construct($nombre,$apellidos,$deporte){
             $this->nombre = $nombre;
@@ -15,46 +16,106 @@
             return $this->nivel."<br/>";
         }
         function introducirResultado($res) {
-            switch(strtolower($res)){
-                case "victoria":
-                    array_push($this->historicoPartidos,"victoria");
-                    echo "Victoria :D<br/>";
-                    $contV = 0;
-                    foreach($this->historicoPartidos as $key => $value){
-                        if($value == "victoria"){
-                            $contV++;
-                            //echo $contV;
-                            if($contV == 6 && $this->nivel != 6){
-                                echo "LEVEL UP!<br/>";
-                                $this->nivel++;
-                            }
-                        }
-                    }
-                    break;
-                case "derrota":
-                    array_push($this->historicoPartidos,"derrota");
-                    echo "Derrota :(<br/>";
-                    $contD = 0;
-                    foreach($this->historicoPartidos as $key => $value){
-                        if($value == "derrota"){
-                            $contD++;
-                            //echo $contD;
-                            if($contD == 6 && $this->nivel != 0){
-                                echo "LEVEL DOWN!<br/>";
-                                $this->nivel--;
-                            }
-                        }
-                    }
-                    break;
-                case "empate":
-                    array_push($this->historicoPartidos,"empate");
-                    echo "Empate :|";
-                    break;
-            }
-            //print_r($this->historicoPartidos);
+            $this->resContV();
+            $this->resContD();
+            $contV = 0;
+            $contD = 0;
+            if(strtolower($res) == "victoria"){
+                
+                array_push($this->historicoPartidos,"victoria");
+                array_push($this->historicoPartidosOG,"victoria");
+                echo "<br/>-----------------Victoria :)<br/>";
 
+                for($i = 0; $i < sizeof($this->historicoPartidos); $i++){
+                    if($this->historicoPartidos[$i] == "victoria"){
+                        //$this->resContD();
+                        
+                        if($i != 0){
+                            if($this->historicoPartidos[$i-1] != "victoria"){
+                                $this->resContV();
+                            }
+                        }else if($i == 0){
+                            $this->resContV();
+                            $this->resContV();
+                        }
+
+                        $lvlCheck = $this->contV();
+                        if($lvlCheck == 6 && $this->nivel != 6){
+                            $this->nivel++;
+                            //print_r($this->historicoPartidos);
+                            echo "<br/>--------------------------LEVEL UP!<br/><br/>";
+                            echo "-----------------Nivel: ".$this->getNivel();
+                            $this->historicoPartidos = array_slice($this->historicoPartidos,$i+1);
+                            $lvlCheck = 0;
+                        }
+                    }
+                }
+                
+            }else if(strtolower($res) == "derrota"){
+                array_push($this->historicoPartidos,"derrota");
+                array_push($this->historicoPartidosOG,"derrota");
+                echo "<br/>-----------------Derrota :(<br/>";
+                for($i = 0; $i < sizeof($this->historicoPartidos); $i++){
+                    if($this->historicoPartidos[$i] == "derrota"){
+                        //$this->resContV();
+                        
+                        if($i != 0){
+                            if($this->historicoPartidos[$i-1] != "derrota"){
+                                $this->resContD();
+                            }
+                        }else if($i == 0){
+                            $this->resContV();
+                            $this->resContV();
+                        }
+                        
+                        $lvlCheck = $this->contD();
+                        if($lvlCheck == 6 && $this->nivel != 0){
+                            $this->nivel--;
+                            //print_r($this->historicoPartidos);
+                            echo "<br/>--------------------------LEVEL DOWN!<br/><br/>";
+                            echo "-----------------Nivel: ".$this->getNivel();
+                            $this->historicoPartidos = array_slice($this->historicoPartidos,$i+1);
+                            $lvlCheck = 0;
+                        }
+                    }
+                }
+                
+            }else if(strtolower($res) == "empate"){
+                $this->resContD();
+                $this->resContV();
+                array_push($this->historicoPartidos,"empate");
+                array_push($this->historicoPartidosOG,"empate");
+                echo "<br/>-----------------Empate :|<br/>";
+
+                $key = array_search("empate", $this->historicoPartidos);
+                $this->historicoPartidos = array_slice($this->historicoPartidos,$key);
+            }
+
+            
+        }
+        private $contV = 0;
+        private $contD = 0;
+        function contV(){
+            $this->contV++;
+            //echo "V: $this->contV</br>";
+            return $this->contV;
+        }
+        function resContV(){
+            $this->contV = 0;
+            //echo "Reset V<br/>";
+            return $this->contV;
         }
 
+        function contD(){
+            $this->contD++;
+            //echo "D: $this->contD<br/>";
+            return $this->contD;
+        }
+        function resContD(){
+            $this->contD = 0;
+            //echo "Reset D<br/>";
+            return $this->contD;
+        }
 
     }
 
@@ -68,7 +129,7 @@
     $us1->introducirResultado("victoria");
     $us1->introducirResultado("victoria");
     
-    echo $us1->getNivel();
+   
     
     $us1->introducirResultado("derrota");
     $us1->introducirResultado("derrota");
@@ -77,8 +138,33 @@
     $us1->introducirResultado("derrota");
     $us1->introducirResultado("derrota");
 
-    echo $us1->getNivel();
+    
 
+    $us1->introducirResultado("victoria");
+    $us1->introducirResultado("victoria");
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("empate");
 
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("derrota");
 
+    $us1->introducirResultado("victoria");
+    $us1->introducirResultado("victoria");
+    $us1->introducirResultado("victoria");
+    $us1->introducirResultado("victoria");
+    $us1->introducirResultado("victoria");
+    $us1->introducirResultado("victoria");
+
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("victoria");
+    $us1->introducirResultado("victoria");
+    $us1->introducirResultado("derrota");
+    $us1->introducirResultado("victoria");
+    
 ?>
