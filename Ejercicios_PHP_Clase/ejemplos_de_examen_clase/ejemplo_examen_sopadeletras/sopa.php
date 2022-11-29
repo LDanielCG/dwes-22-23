06b6b6c5092b3d959821d61e7e690fd1d74f8c47
LDanielCG <luis.daniel.casgui@gmail.com>
2022-11-30 00:10:00 +0100

    Sopa de letras

---
+++ Ejercicios_PHP_Clase/ejemplos_de_examen_clase/ejemplo_examen_sopadeletras/sopa.php
@@ -0,0 +1,33 @@
+<?php
+    require("./Controlador/Tabla.php");
+
+    use Controlador\Tabla as Tabla;
+
+    $board = new Tabla(5, 5);
+    $board->placeWord("Hola");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sopa de Letras</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        td {
+            width: 25px;
+            height: 25px;
+            border: 1px solid black;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <?= $board->printBoard(); ?>
+</body>
+</html>
