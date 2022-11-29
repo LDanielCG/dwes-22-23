c41a6ff9677f54ea5d63011dbb7d0a77f36231ca
LDanielCG <luis.daniel.casgui@gmail.com>
2022-11-29 01:02:04 +0100

    sopaDeLetras NF

--- Ejercicios_PHP_Clase/ejemplos_de_examen_clase/ejemplo_examen_sopadeletras/sopadeletras.php
+++ Ejercicios_PHP_Clase/ejemplos_de_examen_clase/ejemplo_examen_sopadeletras/sopadeletras.php
@@ -1,29 +1,87 @@
 <?php
 
-    $tabl[] = [];
+
+    $tablero = [];
+
     function inicializaSopaLetras($tablero, $n, $m){
-        for($i = 0; $i < $n; $i++){
-            for($j = 0; $j < $m; $j++){
-                $tablero[$i][$j] = chr(rand(ord("a"),ord("z")));
+        for($filas=0; $filas < $n; $filas++) {
+            for($columnas=0; $columnas < $m; $columnas++) {
+                $tablero[$filas][$columnas] = chr(rand(ord("a"),ord("z")));
             }
         }
         return $tablero;
     }
-    $tabl = inicializaSopaLetras($tabl,2,6);
-    print_r($tabl);
-
 
     function pintaTablero($tablero){
-        echo "<table border>";
-        for($i = 0; $i < count($tablero); $i++){
-            echo "<tr>";
-            foreach($tablero[$i] as $key => $value){
-                echo "<td>$value</td>";
+        echo "<table border width='200px'>";
+            foreach($tablero as $filas){
+                echo "<tr>";
+                    foreach($filas as $columnas){
+                        echo "<td>".$columnas."</td>";
+                    }
+                echo "</tr>";
             }
-            echo "</tr>";
-        }
         echo "</table>";
     }
-    pintaTablero($tabl);
+
+    function colocaPalabraHorizontal($tablero, $palabra){
+        $rand_pos_fila = rand(0, (count($tablero))-1);
+        $rand_pos_col = rand(0, (count($tablero[0]))-1);
+        $palabraArr = str_split($palabra);
+
+        print_r($tablero);
+        echo "<br><br><br>";
+        print_r($palabraArr);
+        echo "<br>$rand_pos_col<br>";
+        
+
+        for($filas=0; $filas < count($tablero); $filas++) {
+            if($filas == $rand_pos_fila){
+                for($columnas=0; $columnas < count($tablero[$filas]); $columnas++) {
+
+
+                    if($columnas >= $rand_pos_col){
+                        $tablero[$filas][$columnas] = $palabraArr[$columnas-$rand_pos_col];
+                        if($tablero[$filas][$columnas] == NULL){
+                            $tablero[$filas][$columnas] = chr(rand(ord("a"),ord("z")));
+                        }
+                    }
+                    /*
+                    //Palabra cabe en la fila
+                    if(strlen($palabra) < count($tablero[$filas])){
+
+                        //Inserta la palabra al inicio de la fila
+                        $tablero[$filas][$columnas] = $palabraArr[$columnas];
+
+                        //Si el tamaño de la palabra es menor al de la fila inserta un caracter para rellenarla.
+                        if($palabra[$columnas] == NULL){
+                            $tablero[$filas][$columnas] = chr(rand(ord("a"),ord("z")));
+                        }
+
+                    }else{
+                        //Algo
+                    }*/
+                }
+            }
+        }
+        return $tablero;
+    }
+
+
 
 ?>
+
+
+<html>
+<head>
+    <title>Sopa de letras</title>
+</head>
+<body>
+    <?php 
+        $tablero = inicializaSopaLetras($tablero, 5, 5);
+
+        $tablero = colocaPalabraHorizontal($tablero, "HOLA");
+        pintaTablero($tablero); 
+    ?>
+</body>
+</html>
